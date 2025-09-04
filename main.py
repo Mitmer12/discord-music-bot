8a8730b61f0847a2cf005dc249107cd80d41ee65
Mitmer12 <valosa12@hotmail.com>
2025-09-04 09:14:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -81,23 +81,22 @@ except Exception as e:
     logger.error(f"Spotify ayarları hatası: {e}")
     spotify = None
 
-# YouTube DL ayarları - bu bölümü bulun ve değiştirin
+# YouTube DL ayarları - bu bölümü bulun ve tamamen değiştirin
 ytdl_format_options = {
-    'format': 'bestaudio[ext=m4a]/bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
+    'format': 'bestaudio',
     'noplaylist': True,
     'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
+    'ignoreerrors': True,
     'quiet': True,
     'no_warnings': True,
     'default_search': 'ytsearch',
-    'source_address': '0.0.0.0',
-    'force_json': True,
-    'extract_flat': False,
-    'writethumbnail': False,
-    'writeinfojson': False
+    'extractaudio': True,
+    'audioformat': 'mp3',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'age_limit': 99,
+    'geo_bypass': True,
+    'socket_timeout': 30
 }
 
 # Ses efektleri için FFmpeg ayarları
@@ -169,21 +168,31 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.webpage_url = data.get('webpage_url')
 
     @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False, effect='normal'):
-        loop = loop or asyncio.get_event_loop()
-        try:
-            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-            
-            if 'entries' in data:
-                data = data['entries'][0]
-            
-            filename = data['url'] if stream else ytdl.prepare_filename(data)
-            ffmpeg_options = get_ffmpeg_options(effect)
-            
-            return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-        except Exception as e:
-            logger.error(f"YTDL Hatası: {e}")
-            raise e
+async def from_url(cls, url, *, loop=None, stream=False, effect='normal'):
+    loop = loop or asyncio.get_event_loop()
+    try:
+        # Basit arama terimi olarak işle
+        search_query = f"ytsearch:{url}"
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(search_query, download=False))
+        
+        if 'entries' in data and len(data['entries']) > 0:
+            data = data['entries'][0]
+        else:
+            raise Exception("Video bulunamadı")
+        
+        if not data.get('url'):
+            raise Exception("Video URL'si alınamadı")
+        
+        ffmpeg_options = {
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+            'options': '-vn'
+        }
+        
+        return cls(discord.FFmpegPCMAudio(data['url'], **ffmpeg_options), data=data)
+        
+    except Exception as e:
+        logger.error(f"YTDL Hatası: {e}")
+        raise Exception(f"Şarkı yüklenemedi: {str(e)[:100]}")
 
 def extract_spotify_id(url):
     """Spotify URL'den playlist/track ID'sini çıkar"""
